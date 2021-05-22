aaf808c8a6c468b1ac73aa7aad6949eac62c96c4
Swaraj Paul <swarajpaul29@gmail.com>
2021-05-22 12:26:22 +0530

    Using python-shell

--- scripts/chatbot.js
+++ scripts/chatbot.js
@@ -1,5 +1,6 @@
 const { spawn } = require('child_process');
 const { get } = require('lodash');
+const { PythonShell } = require("python-shell");
 
 const { createError } = require('../controller/error');
 const { getLoggerInstance } = require('../helper/logger');
@@ -12,28 +13,48 @@ fetchChatbotResponse = (req, res, next) => {
     let dataToSend;
     console.log(question);
 
-    // spawn new child process to call the python script
-    const python = spawn('python', ['./scripts/chatbot.py', "\'question\'"]);
-    // collect data from script
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-    });
-    // in close event we are sure that stream from child process is closed
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        console.log(dataToSend)
+    PythonShell.run("./scripts/chatbot.py", { args: [question] }, async (err, result) => {
+        if (err) throw err;
+        const str = result.toString();
+        const response = str.substring(str.indexOf("%"), str.length).trim();
+        console.log(response);
+        // const msg = await switchTag(response, body);
+        // if (msg == "DEFAULT") {
+        //     body.message.message = "We will get back to you within 12-24 hours";
+        //     body.message.sender = body.userTwo;
+        //     addMessage(messageId, body, res, true);
+        // } else {
+        //     body.message.message = msg;
+        //     body.message.sender = body.userTwo;
+        //     addMessage(messageId, body, res, true);
+        // }
         return res.json({
-            result: dataToSend
+            result: response
         })
     });
 
-    python.on('error', (e) => {
-        logger.error(e);
-        let err = createError(`Fetching bot response failed`, 500, ERRORCODE.UNKNOWN_ERROR);
-        return next(err);
-    })
+    // // spawn new child process to call the python script
+    // const python = spawn('python', ['./scripts/chatbot.py', question]);
+    // // collect data from script
+    // python.stdout.on('data', function (data) {
+    //     console.log('Pipe data from python script ...');
+    //     dataToSend = data.toString();
+    // });
+    // // in close event we are sure that stream from child process is closed
+    // python.on('close', (code) => {
+    //     console.log(`child process close all stdio with code ${code}`);
+    //     // send data to browser
+    //     console.log(dataToSend)
+    //     return res.json({
+    //         result: dataToSend
+    //     })
+    // });
+
+    // python.on('error', (e) => {
+    //     logger.error(e);
+    //     let err = createError(`Fetching bot response failed`, 500, ERRORCODE.UNKNOWN_ERROR);
+    //     return next(err);
+    // })
 
 }
 
